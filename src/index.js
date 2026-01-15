73be18c0a3f9b6a1b1b140bb23f1578358709ab6
Zohaib <ranjha5054@gmail.com>
2026-01-15 16:17:03 +0500

    all setup config done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import dotenv from 'dotenv'
-dotenv.config()
-
 import connectDB from './db/index.js';
+import { app } from './app.js';
+dotenv.config({
+    path: "./env"
+})
+
 
 connectDB()
 .then(() => {
